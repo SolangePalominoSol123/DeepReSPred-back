231bd62b2a6644b441888fb59fb9048d4bd19c7c
Ubuntu <ubuntu@ip-172-31-34-241.ec2.internal>
2021-11-29 23:24:37 +0000

    allow change aws s3 credentials on running

--- back_project/s3config.py
+++ back_project/s3config.py
@@ -1,7 +1,19 @@
-from constants import keys
+#from constants import keys
 import boto3
+import json
+
+#A json file is used due to allow aws credentials change after Back-end services were deployed
+def gettingCredentialsFromJson():
+    f = open('constants_aws.json')
+    data = json.load(f)
+
+    credentials=data['keys']
+    f.close()
+    return credentials
 
 def getS3config():
+
+    keys = gettingCredentialsFromJson()
     s3 = boto3.client('s3',
                         aws_access_key_id = keys["ACCESS_KEY_ID"],
                         aws_secret_access_key = keys["ACCESS_SECRET_KEY"],
