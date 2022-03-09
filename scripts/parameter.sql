8216449dd57ab5fc156d39d4ee02fab7f22d33b8
unknown <spalomino@pucp.edu.pe>
2022-03-09 16:57:00 -0500

    initial scripts

---
+++ scripts/parameter.sql
@@ -0,0 +1,3 @@
+INSERT INTO deeprespredDB.parameter (idParameter, name, description, value) VALUES (1, 'maxRequestsxDay', 'Maximum quantity of requests by user per day', 30);
+INSERT INTO deeprespredDB.parameter (idParameter, name, description, value) VALUES (2, 'maxResidues', 'Maximum quantity of residues in a sequence', 515);
+INSERT INTO deeprespredDB.parameter (idParameter, name, description, value) VALUES (3, 'cpuNumber', 'Quantity of server cpus assigned to prediction process', 4);
---
+++ scripts/statusrequest.sql
@@ -0,0 +1,4 @@
+INSERT INTO deeprespredDB.statusrequest (idStatus, name) VALUES (1, 'REGISTERED');
+INSERT INTO deeprespredDB.statusrequest (idStatus, name) VALUES (3, 'PROCESSING');
+INSERT INTO deeprespredDB.statusrequest (idStatus, name) VALUES (4, 'FINALIZED');
+INSERT INTO deeprespredDB.statusrequest (idStatus, name) VALUES (5, 'ERROR');
